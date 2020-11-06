9d84f408bc9c099b5ec60ab89618e34ebc712b4b
Axra98 <axel.ramhult@gmail.com>
2020-11-06 14:36:52 +0100

    2

--- src/Car.java
+++ src/Car.java
@@ -1,15 +1,19 @@
 import java.awt.*;
 
 public class Car implements Movable{
+
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
     public String modelName; // The car model name
+    int dir, x, y;
+    Point pos = new Point(x, y);
 
     public int getNrDoors(){
         return nrDoors;
     }
+
     public double getEnginePower(){
         return enginePower;
     }
@@ -40,14 +44,27 @@ public class Car implements Movable{
     }
 
     public void move() {
-
+        if(dir == 0)
+            pos.move(x, y+1);
+        if(dir == 1)
+            pos.move(x+1, y);
+        if(dir == 2)
+            pos.move(x, y-1);
+        if(dir == 3)
+            pos.move(x-1, y);
     }
 
     public void turnLeft() {
-
+        if(dir == 0)
+            dir = 3;
+        else
+            dir = dir-1;
     }
 
     public void turnRight() {
-
+        if(dir == 3)
+            dir = 0;
+        else
+            dir = dir+1;
     }
 }
