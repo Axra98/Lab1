8da4806a208cfe8c2f2f880b4827fa1e355f8bc5
antpette <antonp98@live.se>
2020-11-17 15:02:56 +0100

    Test

--- src/Movable.java
+++
@@ -1,9 +0,0 @@
-public interface Movable {
-    void move();
-    void turnLeft();
-    void turnRight();
-}
-/**
-Används för att kunna implementera i andra klasser. Vill man använda move, turnLeft och turnRight i
-en annan klass, tex. Cykel så kan
- */
